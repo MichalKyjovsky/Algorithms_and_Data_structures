ea974d3d5c223acae7ddbbac2880c7797a161436
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2019-10-22 12:01:59 +0200

    ...

---
+++ Algorithms/KMP_Search_Alg.py
@@ -0,0 +1,48 @@
+def KMPSearch(pat, txt):
+    M = len(pat) 
+    N = len(txt)
+    
+    #create lps[] that will hold the longest prefix suffix
+    #values for pattern
+    lps = [0]*M 
+    j = 0 #index for pat[]
+
+    computeLPSArray(pat, M, lps)
+
+    i = 0 #index for txt[]
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+
+        if j == M:
+            print ("Found pattern at index " + str(i-j))
+            j = lps[j-1]
+
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+
+def computeLPSArray(pat, M, lps):
+    len = 0 #length of the previous longest prefix suffix 
+    lps[0] = 0
+    i = 1
+
+    while i < M:
+        if pat[i] == pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
+
+txt = "ABABDABACDABABCABAB"
+pat = "ABABCABAB"
+KMPSearch(pat, txt) 
+
